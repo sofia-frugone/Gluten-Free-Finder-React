3fa2ed0a220b2a0165bd88a4a953aa60b41ba97e
sofia_frugone <sofiabellaf2000@gmail.com>
2022-11-14 13:26:40 +1100

    implemented routes for home and signup/in screens

--- src/App.js
+++ src/App.js
@@ -1,81 +1,59 @@
 import './App.css';
-import Navbar from './components/Navbar'
-import FormInput from './components/FormInputs'
-import { useState } from "react";
+import SignUp from './components/SignUp'
+import Home from './components/Home'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+
+
+
 
 
 
 
 const App = () => {
 
-  const [values, setValues] = useState({
-    email: "",
-    password: "",
-    confirmPassword:"",
-  });
 
-  const inputs = [
-    {
-      id:1,
-      name:"email",
-      type:"text",
-      placeholder:"Email",
-      errorMessage: "It should be a valid email address",
-      label:"Email",
-      required: true,
-    },
-
-    {
-      id:2,
-      name:"password",
-      type:"password",
-      placeholder:"Password",
-      errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character",
-      label:"Password",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-      required: true,
-    },
-
-    {
-      id:3,
-      name:"confirmPassword",
-      type:"password",
-      placeholder:"Confirm Password",
-      errorMessage: "Passwords should match",
-      label:"Confirm Password",
-      pattern: values.password,
-      required: true,
-    }
-    
-  ]
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
-
-  const onChange = (e) => {
-    setValues({...values, [e.target.name]: e.target.value});
-  };
-
-  console.log(values);
 
   return (
-    <div className="App">
-      <Navbar />
-  
-      <div className="formInputs">
-        <form onSubmit={handleSubmit}>
-          <h1>Sign Up</h1>
-          {inputs.map((input) => (
-            <FormInput className="inputs" key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
-          ))}
-          <input className="submit" type="submit" />
-        </form>
-      </div>
 
-    </div>
-   
- 
+    
+      <div className="App">
+        
+
+        <BrowserRouter>
+        <nav className="navbar">
+          <div className="logo">
+            <img src="./gff-logo.svg" alt="navbar logo" />
+          </div>
+
+          <h1>Gluten Free Finder</h1>
+          <div className="links">
+            <Link to="/">Home</Link> 
+            <Link to="/signin">Sign In</Link>  
+            <Link to="/signup">Sign Up</Link>           
+          </div>
+        </nav>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/signup" element={<SignUp />}/>
+
+          </Routes>
+        </BrowserRouter>
+
+        
+
+          
+
+
+         
+         
+         
+            
+            
+
+          
+      </div>
+    
   );
 }
 
