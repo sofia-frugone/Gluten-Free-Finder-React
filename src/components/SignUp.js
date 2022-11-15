e95a391e256e77a26bbe1aa4e53f41551ef73012
sofia_frugone <sofiabellaf2000@gmail.com>
2022-11-15 14:29:00 +1100

    implemented sign in screen

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,6 @@
 import FormInput from '../components/FormInputs'
 import { useState } from "react";
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
     const [values, setValues] = useState({
@@ -13,7 +14,7 @@ const SignUp = () => {
           id:1,
           name:"email",
           type:"text",
-          placeholder:"Email",
+          placeholder:"Email*",
           errorMessage: "It should be a valid email address",
           label:"Email",
           required: true,
@@ -23,7 +24,7 @@ const SignUp = () => {
           id:2,
           name:"password",
           type:"password",
-          placeholder:"Password",
+          placeholder:"Password*",
           errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character",
           label:"Password",
           pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
@@ -34,7 +35,7 @@ const SignUp = () => {
           id:3,
           name:"confirmPassword",
           type:"password",
-          placeholder:"Confirm Password",
+          placeholder:"Confirm Password*",
           errorMessage: "Passwords should match",
           label:"Confirm Password",
           pattern: values.password,
@@ -55,16 +56,29 @@ const SignUp = () => {
 
     return (  
         <div className="formInputs">
-               <form onSubmit={handleSubmit}>
-                 <h1>Sign Up</h1>
-                 {inputs.map((input) => (
-                   <FormInput className="inputs" key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
-                  ))}
-                  <input className="submit" type="submit" />
-               </form>
-             </div>
 
+            <div className='signUpMessage'>
+               <h2>Want to list your restaurant on Gluten Free Finder?</h2>
+               <p>This is simply dummy text of the printing and typesetting industry. That has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Layout passages. This is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+
+
+            <form onSubmit={handleSubmit}>
+                <h1>Sign Up</h1>
+                {inputs.map((input) => (
+                  <FormInput className="inputs" key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+                ))}
+                <input className="submit" type="submit" />
+                <Link to="/signin"><p>Already a user? Sign In</p></Link>
+             </form>
+
+             
+            
+         </div>
+
+         
     );
+    
 }
  
 export default SignUp;
